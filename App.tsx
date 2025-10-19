1594034c6afb0b2bd1a216ca3c577ad5063d9ebb
Yash Shah <62358049+git-yash@users.noreply.github.com>
2025-10-18 21:18:19 -0500

    changed status bar style

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React, { createContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import UseWelcome from "./OnboardingScreens/Welcome/useWelcome";
 import InitialAssessment from "./OnboardingScreens/InitialAssessment/InitialAssessment";
 import {StatusBar} from "react-native";
+import Welcome from "./OnboardingScreens/Welcome/Welcome";
 
 const Stack = createNativeStackNavigator();
 
@@ -29,9 +29,9 @@ export default function App() {
     return (
         <ThemeContext.Provider value={theme}>
             <NavigationContainer>
-                <StatusBar barStyle='dark-content' />
-                <Stack.Navigator initialRouteName="UseWelcome">
-                    <Stack.Screen name="Welcome" component={UseWelcome} options={{ headerShown: false }} />
+                <StatusBar barStyle='light-content' />
+                <Stack.Navigator initialRouteName="Welcome">
+                    <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
                     <Stack.Screen name="Initial Assessment" component={InitialAssessment} options={{ headerShown: true }} />
                 </Stack.Navigator>
             </NavigationContainer>
